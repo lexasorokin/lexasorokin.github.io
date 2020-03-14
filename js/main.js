bbb313fa7748c1cf09862514ceb1be22c12fa78f
lexasorokin <sorokin.developer@gmail.com>
2020-03-14 10:19:35 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function () {
+
+
+    //faq items mechanics
+    $('.faq_item_content').not('.faq_item.active .faq_item_content').slideUp(0);
+
+    $('.faq_item_head a').click(function (e) {
+        e.preventDefault();
+        $(this).closest('.faq_block').find('.faq_item').not($(this).closest('.faq_item')).removeClass('active');
+        $(this).closest('.faq_block').find('.faq_item').not($(this).closest('.faq_item')).find('.faq_item_content').slideUp();
+
+        $(this).closest('.faq_item').toggleClass('active');
+        $(this).closest('.faq_item').find('.faq_item_content').slideToggle();
+    });
+
+
+    $('.zeen-slider-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        cssEase: 'ease',
+        focusOnSelect: true,
+        dots: false,
+        autoplay: false,
+        slide: ".zeen-item",
+        appendArrows: $('.zeen-slider-buttons'),
+        prevArrow: '<button class="zeen-prev" aria-label="Previous" type="button" style="">Previous</button>',
+        nextArrow: '<button class="zeen-next" aria-label="Next" type="button" style="">Next</button>',
+        //asNavFor: '.zeen-slider-wrapper .zeen-item .slick-flex .btn',
+        respondTo: 'slider',
+        swipeToSlide: true,
+        variableWidth: false,
+        touchThreshold: 25,
+        pauseOnHover: false,
+    });
+
+    setInterval(function () { $('.svg-glitch').toggleClass('glitch-active') }, 4000);
+
+
+    $(".action-buttons a").click(function () {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 500);
+        return false;
+    });
+
+});
+
+
---
+++ send.php
@@ -0,0 +1,38 @@
+<?php
+$name = $_POST['name'];
+$company = $_POST['company'];
+$email = $_POST['email'];
+$question = $_POST['question'];
+
+$name = htmlspecialchars($name);
+$company = htmlspecialchars($company);
+$email = htmlspecialchars($email);
+$question = htmlspecialchars($question);
+
+$name = urldecode($name);
+$company = urldecode($company);
+$email = urldecode($email);
+$question = urldecode($question);
+
+$name = trim($name);
+$company = trim($company);
+$email = trim($email);
+$question = trim($question);
+//echo $name;
+//echo "<br>";
+//echo $company;
+//echo "<br>";
+//echo $email;
+//echo "<br>";
+//echo $question;
+
+
+if (mail("zeen@eventum-premo.ru", "Заявка с сайта Zeen", "ФИО:".$name."  Компания:".$company.".  E-mail: ".$email." Описание задачи:".$question ,"From: info@eventum-premo.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
+
+//name, company, email, question
+?>
+
