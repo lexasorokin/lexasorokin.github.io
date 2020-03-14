b7af32c067a08e736c42bf0399aab153e494087a
lexasorokin <sorokin.developer@gmail.com>
2020-03-14 20:31:13 +0100

    slider adaptive

--- js/main.js
+++ js/main.js
@@ -36,6 +36,27 @@ $(document).ready(function () {
         pauseOnHover: false,
     });
 
+    $('.zeen-slider-wrapper-mobile').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        cssEase: 'ease',
+        focusOnSelect: true,
+        dots: false,
+        autoplay: false,
+        slide: ".zeen-item",
+        appendArrows: $('.zeen-slider-buttons-mobile'),
+        prevArrow: '<button class="zeen-prev" aria-label="Previous" type="button" style="">Previous</button>',
+        nextArrow: '<button class="zeen-next" aria-label="Next" type="button" style="">Next</button>',
+        //asNavFor: '.zeen-slider-wrapper .zeen-item .slick-flex .btn',
+        respondTo: 'slider',
+        swipeToSlide: true,
+        variableWidth: false,
+        touchThreshold: 25,
+        pauseOnHover: false,
+    });
+
     setInterval(function () { $('.svg-glitch').toggleClass('glitch-active') }, 4000);
 
 
